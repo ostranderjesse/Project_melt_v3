68c8853c47efc1a62a458378d4a52ba0920fc101
ostranderjesse <62621928+ostranderjesse@users.noreply.github.com>
2022-07-27 16:47:00 -0400

    created follow camera
    Cinemachine now follows the prefab player, still need to add camera features when warping but good start so far.

---
+++ Melt_v3/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+using Cinemachine;
+
+public class CameraFollow : MonoBehaviour
+{
+    #region variables
+
+    public GameObject tPlayer;
+    public Transform tfollowTarget;
+    [SerializeField] private CinemachineVirtualCamera vcam;
+
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+        tPlayer = GameObject.FindGameObjectWithTag("Player");
+
+        if( vcam == null)
+        {
+            vcam = GetComponent<CinemachineVirtualCamera>();
+        }
+
+        if(tPlayer == null)
+        {
+            tPlayer = GameObject.FindGameObjectWithTag("Player");
+        }
+        tfollowTarget = tPlayer.transform;
+        vcam.LookAt = tfollowTarget;
+        vcam.Follow = tfollowTarget;
+
+    }
+}
