b5ce5548618d96d5dd3131b7e6685971c92f0c2c
ostranderjesse <62621928+ostranderjesse@users.noreply.github.com>
2022-11-08 10:38:54 -0500

    Created Score System
    created a base score system to use for buying items. ( needs to be implemented in all scenes needing score.

---
+++ Melt_v3/Assets/Scripts/coin collecting/CollectCoins.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectCoins : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    public static int theScore;
+
+
+    private void Awake()
+    {
+        theScore = PlayerPrefs.GetInt("numberofCoins",0); //defualt value
+    }
+
+    private void Update()
+    {
+        //change the scroe
+        scoreText.GetComponent<Text>().text = theScore.ToString();
+
+
+    }
+}
---
+++ Melt_v3/Assets/Scripts/coin collecting/CollectStar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectStar : MonoBehaviour
+{
+    //public AudioSource collectSound;
+
+  // [SerializeField] private CollectCoins collectCoinsRef;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //play sound
+        //collectSound.Play();
+        //change score
+       // collectCoinsRef = GetComponent<CollectCoins>();
+
+        CollectCoins.theScore += 50;
+
+        PlayerPrefs.SetInt("numberofCoins", CollectCoins.theScore);
+
+        Destroy(gameObject);
+
+    }
+}
