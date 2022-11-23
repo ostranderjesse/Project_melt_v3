ead62da08766c4fe6126276d99d9d0b15a740f81
ostranderjesse <62621928+ostranderjesse@users.noreply.github.com>
2022-11-23 12:48:46 -0500

    Started the Shop
    made the basic layout of the store, Tried adding an animation to the item. this needs fixing. hover over item to start animation, stop if not overing. click to go into stats all that need to be created.

---
+++ Melt_v3/Assets/Scripts/UI Scripts/HoverTextScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HoverTextScript : MonoBehaviour
+{
+    public Text textToMove;
+    public float timePassed;
+
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.Rotate(Vector3.left * speed * Time.deltaTime);
+
+        //float y = Mathf.PingPong(Time.time * speed, 1) * 6 - 3;
+
+        //textToMove.transform.position = new Vector3(0, y, 0);
+     
+
+      // textToMove.transform.position = new Vector3(0, y, 0);
+        //transform.Rotate(0, y, 0);
+
+    }
+
+}
---
+++ Melt_v3/Assets/Scripts/UI Scripts/ShopManagerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopManagerScript : MonoBehaviour
+{
+    public Text textObject1;
+   // public Text textObject2;
+    //public Text textObject3;
+    public Animation anim;
+  
+
+    public void Start()
+    {
+
+        anim = textObject1.GetComponent<Animation>();
+        
+
+        if(textObject1.GetComponent<Animation>() != null)
+        Debug.Log("animator found");
+
+        anim.Play("TextMovementAnimation");
+ 
+
+    }
+
+    private void Update()
+    {
+        if (textObject1.GetComponent<Animation>() != null)
+            Debug.Log("animator ! found");
+
+      
+
+    }
+
+    public void OnMouseOver()
+    {
+        //play animation
+        anim.Play("TextMovementAnimation");
+
+    }
+
+    public void OnMouseExit()
+    {
+        //stop animation
+       // anim1.Stop("TextMovementAnimation");
+     
+    }
+}
