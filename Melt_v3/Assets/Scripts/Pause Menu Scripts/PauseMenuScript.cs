61f12a78872058d4b2214906b14b496a8c5bb4f4
ostranderjesse <62621928+ostranderjesse@users.noreply.github.com>
2022-12-23 10:59:45 -0500

    Created Pause Menu System
    this system needs to be added to each level, A main Menu has to be created as well for the pause system to fully work.

---
+++ Melt_v3/Assets/Scripts/Pause Menu Scripts/PauseMenuScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public static bool GamegameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GamegameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamegameIsPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamegameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading main menu");
+        Time.timeScale = 1f;
+        //SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Game has been Quit");
+        Application.Quit();
+    }
+
+
+
+
+}
